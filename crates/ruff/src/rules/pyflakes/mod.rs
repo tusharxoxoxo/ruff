e54f926283c463f5469ff38c4addc8db3dd24565
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-17 14:21:16 -0400

    Move unused deletion tracking to end-of-file

--- crates/ruff/src/rules/pyflakes/mod.rs
+++ crates/ruff/src/rules/pyflakes/mod.rs
@@ -2114,7 +2114,7 @@ mod tests {
         try: pass
         except Exception as fu: pass
         "#,
-            &[Rule::UnusedVariable, Rule::RedefinedWhileUnused],
+            &[Rule::RedefinedWhileUnused, Rule::UnusedVariable],
         );
     }
 
